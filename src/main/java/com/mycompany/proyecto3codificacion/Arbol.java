ba2c3603dcdaf437afe43b91dbdb8418081d5e1e
Tato <tatobig@gmail.com>
2020-11-15 23:10:03 -0600

    Correciones

--- src/main/java/com/mycompany/proyecto3codificacion/Arbol.java
+++ src/main/java/com/mycompany/proyecto3codificacion/Arbol.java
@@ -14,8 +14,10 @@ import java.util.PriorityQueue;
  * @author aniba
  */
 public class Arbol {
+
     private Nodo raiz;
     private ArrayList<Nodo> codigoBits;
+
     public Nodo getRaiz() {
         return raiz;
     }
@@ -31,7 +33,7 @@ public class Arbol {
     public void setCodigoBits(ArrayList<Nodo> codigoBits) {
         this.codigoBits = codigoBits;
     }
-    
+
     public Arbol() {
         raiz = null;
     }
@@ -39,36 +41,38 @@ public class Arbol {
     public Arbol(Nodo raizArbol) {
         raiz = raizArbol;
     }
+
     //Mira pue tato, aca ya esta el arbol el char es la letra que se tiene y el integer es la cantidad de veces que aparece dicha letra.
     public void insertar(ArrayList<Letras> letrasFreq) {
         int n = letrasFreq.size();
         PriorityQueue<Nodo> arbolHuffman = new PriorityQueue<Nodo>(n, new Comparador());
 
         for (int i = 0; i < n; i++) {
-          Nodo nodo = new Nodo();
-          nodo.setLetra(letrasFreq.get(i).getLetra());
-          nodo.setDato(letrasFreq.get(i).getRepeticiones());
-          nodo.setIzq(null);
-          nodo.setDer(null);
-          arbolHuffman.add(nodo);
+            Nodo nodo = new Nodo();
+            nodo.setLetra(letrasFreq.get(i).getLetra());
+            nodo.setDato(letrasFreq.get(i).getRepeticiones());
+            nodo.setIzq(null);
+            nodo.setDer(null);
+            arbolHuffman.add(nodo);
         }
         Nodo raiz = null;
         while (arbolHuffman.size() > 1) {
-          Nodo nodoIzq = arbolHuffman.peek();
-          arbolHuffman.poll();
-          Nodo nodoDer = arbolHuffman.peek();
-          arbolHuffman.poll();
-          Nodo nuevo = new Nodo();
-          nuevo.setDato(nodoIzq.getDato()+nodoDer.getDato());
-          nuevo.setLetra('0');
-          nuevo.setIzq(nodoIzq);
-          nuevo.setDer(nodoDer);
-          raiz = nuevo;
-          arbolHuffman.add(nuevo);
+            Nodo nodoIzq = arbolHuffman.peek();
+            arbolHuffman.poll();
+            Nodo nodoDer = arbolHuffman.peek();
+            arbolHuffman.poll();
+            Nodo nuevo = new Nodo();
+            nuevo.setDato(nodoIzq.getDato() + nodoDer.getDato());
+            nuevo.setLetra('*');
+            nuevo.setIzq(nodoIzq);
+            nuevo.setDer(nodoDer);
+            raiz = nuevo;
+            arbolHuffman.add(nuevo);
         }
         this.codigoBits = new ArrayList<>();
         obtenerCodigoBits(raiz, "");
     }
+
     public String toString() {
         return raiz.toString();
     }
@@ -76,25 +80,28 @@ public class Arbol {
     public void enOrder(Nodo nodoRaiz) {
         if (nodoRaiz != null) {
             enOrder(nodoRaiz.getIzq());
-            if(nodoRaiz.getDer()==null&&nodoRaiz.getIzq()==null){
+            if (nodoRaiz.getDer() == null && nodoRaiz.getIzq() == null) {
                 System.out.println(nodoRaiz.toString());
             }
             enOrder(nodoRaiz.getDer());
         }
     }
+
     public void obtenerCodigoBits(Nodo raiz, String cadena) {
-    if (raiz.getIzq() == null && raiz.getDer() == null) {
-        System.out.println(raiz.getLetra() + "   |  " + cadena);
-        raiz.setCodigo(cadena);
-        this.codigoBits.add(raiz);
-        return;
+        if (raiz.getIzq() == null && raiz.getDer() == null) {
+            System.out.println(raiz.getLetra() + "   |  " + cadena);
+            raiz.setCodigo(cadena);
+            this.codigoBits.add(raiz);
+            return;
+        }
+        obtenerCodigoBits(raiz.getIzq(), cadena + "0");
+        obtenerCodigoBits(raiz.getDer(), cadena + "1");
     }
-    obtenerCodigoBits(raiz.getIzq(), cadena + "0");
-    obtenerCodigoBits(raiz.getDer(), cadena + "1");
-  }
 }
+
 class Comparador implements Comparator<Nodo> {
-  public int compare(Nodo nodoIzq, Nodo nodoDer) {
-    return nodoIzq.getDato()- nodoDer.getDato();
-  }
+
+    public int compare(Nodo nodoIzq, Nodo nodoDer) {
+        return nodoIzq.getDato() - nodoDer.getDato();
+    }
 }
